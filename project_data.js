b6801b76b8c70c6018718aa4d4541214f539b8e2
Presthon <jackprestondaly11@gmail.com>
2024-04-04 00:24:24 -0500

    spelling mistakes + added background for modal

--- project_data.js
+++ project_data.js
@@ -2,66 +2,66 @@ const ProjectData = {
 	"cedar": {
 		title: "CEDAR",
 		github: "https://github.com/Presthon",
-		description: "CEDAR is the accumulation of ~5 months of work during my Cybersecurity Capstone class at DePaul was worth almost my entire grade for the class (A :]). The ask of this project was to completley redesign the entire IT infasture of a three office company called CEDAR. From running cables, to do firewall configurations, to email security, we did it all. This project overlapped with my time working for Solerity and I actually was able to use a lot of knowledge I gained from CEDAR to better understand Solerity's infastrcutre, and vice versa!"
+		description: "CEDAR is the accumulation of ~5 months of work during my Cybersecurity Capstone class at DePaul and was worth almost my entire grade for the class (A :]). The ask of this project was to completely redesign the entire IT infrastructure of a three-office company called CEDAR. From running cables to firewall configurations to email security, we did it all. This project overlapped with my time working for Solerity, and I actually was able to use a lot of knowledge I gained from CEDAR to better understand Solerity's infrastructure, and vice versa!"
 	},
 	"tryhackme": {
 		title: "Tryhackme",
 		github: "",
-		description: "Tryhack is a great tool that I use quite often to practice my cybersecurity skills. I am currently on pace to hit top 1% this year and hope to do so! Offensive security practice is extremley important because it forces you to think like a hacker, so that when you are protecting enviornments, you know what to patch."
+		description: "Tryhackme is a great tool that I use quite often to practice my cybersecurity skills. I am currently on pace to hit the top 1% this year and hope to do so! Offensive security practice is extremely important because it forces you to think like a hacker so that when you are protecting environments, you know what to patch."
 	},
 	"homelab": {
-		title: "Custom made home lab",
+		title: "Custom-made home lab",
 		github: "https://github.com/Presthon",
-		description: "My multi-purpose security home lab is one of my most prized possessions! What started off as an HP Omen laptop, is now a frankenstien machine with custom networking gear, SSDs, upgraded ram, and whole plethora of over small tweaks. It runs like a charm and I use it for thins like pen-testing, networking, programming, and just about everything I am able to use it for!"
+		description: "My multi-purpose security home lab is one of my most prized possessions! What started off as an HP Omen laptop is now a Frankenstein machine with custom networking gear, SSDs, upgraded RAM, and a whole plethora of other small tweaks. It runs like a charm, and I use it for things like pen-testing, networking, programming, and just about everything I am able to use it for!"
 	},
 	"website": {
 		title: "My website!",
 		github: "https://github.com/Presthon",
-		description: "You're looking at it! I am super proud of how this website turned out and plan to make more websites in the future because of how much fun I had making it. I maade this website in the span of around ~24 hours, and had almost no prior experience with the HTML/CSS/JS stack. After this development marathon, I feel way more confident with this stack and plan on getting better and better!"
+		description: "You're looking at it! I am super proud of how this website turned out and plan to make more websites in the future because of how much fun I had making it. I made this website in the span of around ~24 hours and had almost no prior experience with the HTML/CSS/JS stack. After this development marathon, I feel way more confident with this stack and plan on getting better and better!"
 	},
 	"avendra": {
 		title: "Avendra, by Aramark",
 		github: "https://github.com/Presthon",
-		description: "Although my time working at Avendra was much shorter than my time with Solerity, it was still very impactful in my data science education. I worked on a team of Data Science professionals and wrote predictive models aimed at re-using old data to predict future trends in customer buying patterns. The workflow went as follows: I individually wrote a lot of these models, passed them to my coworker's who looked it over and gave me feedback on things I could change. "
+		description: "Although my time working at Avendra was much shorter than my time with Solerity, it was still very impactful on my data science education. I worked on a team of Data Science professionals and wrote predictive models aimed at reusing old data to predict future trends in customer buying patterns. The workflow went as follows: I individually wrote a lot of these models, passed them to my coworkers who looked them over and gave me feedback on things I could change."
 	},
 	"nnetwork": {
-		title: "Nureal Networks",
+		title: "Neural Networks",
 		github: "https://github.com/Presthon",
-		description: "One of the fields that I grew very intersted in with my Python studies was Neural Networks. After a long time of writing more traditonal machine learning models, I ventured into Neural Networks and saw just how powerful they can be. Of course, any good data scientist has to include this disclaimer: there is no 'best' model, its all situational. I just particularly enjoy working with these models."
+		description: "One of the fields that I grew very interested in with my Python studies was Neural Networks. After a long time of writing more traditional machine learning models, I ventured into Neural Networks and saw just how powerful they can be. Of course, any good data scientist has to include this disclaimer: there is no 'best' model, it's all situational. I just particularly enjoy working with these models."
 	},
 	"solerity": {
 		title: "Solerity - Federal Contracting",
 		github: "https://github.com/Presthon",
-		description: "I worked for Solerity for ~2 years and learned more than I could ever learn in school! Although my title was 'Cybersecurity Intern' I think my coworkers would agree that I played a much bigger role than a typical intern. My main task there was to oversee Soleirty's effort to ensure compliance with NIST 800 series standards as well as CMMC Level 2."
+		description: "I worked for Solerity for ~2 years and learned more than I could ever learn in school! Although my title was 'Cybersecurity Intern,' I think my coworkers would agree that I played a much bigger role than a typical intern. My main task there was to oversee Solerity's effort to ensure compliance with NIST 800 series standards as well as CMMC Level 2."
 	},
 	"viz": {
 		title: "Visualization",
 		github: "https://github.com/Presthon",
-		description: "Visualization in python is something that I have grown to love. I'll admit, my first ventures with matplotlib did not entice me too much, after using things like Plotly, Seaborn, and Bokeh, I learned how much power you have as a Python dev in terms of visualization."
+		description: "Visualization in Python is something that I have grown to love. I'll admit, my first ventures with matplotlib did not entice me too much. After using things like Plotly, Seaborn, and Bokeh, I learned how much power you have as a Python dev in terms of visualization."
 	},
 	"7on11": {
 		title: "7 on 11",
 		github: "https://github.com/Presthon",
-		description: "7 on 11 holds a special place in my heart as my first (semi-real) hacking project. It was an assignment in my Cybersecurity capstone class made by one of my favorite professors, and to be honest it really pushed me! I gave it my all and am honestly still proud of how it turned out!",
+		description: "7 on 11 holds a special place in my heart as my first (semi-real) hacking project. It was an assignment in my Cybersecurity capstone class made by one of my favorite professors, and to be honest, it really pushed me! I gave it my all and am honestly still proud of how it turned out!",
 	},
 	"tutor": {
-		title: "Tutoring Buisness",
+		title: "Tutoring Business",
 		github: "https://github.com/Presthon",
-		description: "For about 6 years spread out between high school and collge, I did a lot of compter science tutoring, both freelance and at DePaul. I worked as a teachers aid for a year for children in 8th grade and since then realized that I actually like teaching people, so I decided teaching what I am best at, computer science, would be a great way to excersise this passion. I stopped once I graduated with my undergrad, bust stil help anyone who asks!",
+		description: "For about 6 years spread out between high school and college, I did a lot of computer science tutoring, both freelance and at DePaul. I worked as a teacher's aid for a year for children in 8th grade and since then realized that I actually like teaching people, so I decided teaching what I am best at, computer science, would be a great way to exercise this passion. I stopped once I graduated with my undergrad but still help anyone who asks!",
 	},
 	"podcast": {
 		title: "Podcast",
 		github: "https://github.com/Presthon",
-		description: "I talk about stuff. if you are looking at this, ill give you $1",
+		description: "I talk about stuff. If you are looking at this, I'll give you $1",
 	},
 	"hackerone": {
-		title: "hackerone",
+		title: "Hackerone",
 		github: "https://github.com/Presthon",
-		description: "Hackerone in my mind is my 'next step' in pen-testing. It hosts real bug bounties given out by real companies, and some people make a living off this website. It's becoming a much bigger focus for me as all my other cybersecurity studies are helpful, but Hackerone is modern day, legitamte scenarios.",
+		description: "Hackerone in my mind is my 'next step' in pen-testing. It hosts real bug bounties given out by real companies, and some people make a living off this website. It's becoming a much bigger focus for me as all my other cybersecurity studies are helpful, but Hackerone is a modern-day, legitimate scenario.",
 	},
 	"bomblab": {
 		title: "The Bomb Lab",
 		github: "https://github.com/Presthon",
-		description: "The bomb lab is one of the most famous computer science projects that most CS students have to face. Its a reverse engineering project were you have to step through lines of assembly code and find what secret password will stop a number of assembly 'bombs' from blowing up. I was able to get a perfect score on this project and had a ton of fun doing it!",
+		description: "The bomb lab is one of the most famous computer science projects that most CS students have to face. It's a reverse engineering project where you have to step through lines of assembly code and find what secret password will stop a number of assembly 'bombs' from blowing up. I was able to get a perfect score on this project and had a ton of fun doing it!",
 	},
 };
